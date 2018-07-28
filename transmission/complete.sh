f927899dbedab6126ddbf803f1eb33f3766998e1
Lukas Andersson <zather1995@gmail.com>
2018-07-28 20:39:34 +0200

    added complete script

---
+++ transmission/complete.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#exec &> /tmp/logfile.txt
+
+export HOME=/config
+
+sleep 1
+
+filebot -script fn:amc \
+	--output "/data/videos/" \
+	--def animeFormat="Anime/{n}/{n}.S{(episode.season ? s : 1).pad(2)}E{e.pad(2)}.{t}" \
+	--def movieFormat="Movies/{n} ({y})/{n} ({y})" \
+	--log-file amc.log \
+	--action copy \
+	-non-strict "/data/torrents/completed" \
+	--def excludeList=amc.txt \
+	--def "exec=chown 1000:1000 -R '{output}'"
+
