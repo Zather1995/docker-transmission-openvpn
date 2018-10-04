236431f635d4a3f58293e570d418080765f71f12
Zather1995 <zather1995@gmail.com>
2018-10-04 02:27:24 +0200

    Correct filebot script

--- transmission/complete.sh
+++ transmission/complete.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-#exec &> /tmp/logfile.txt
-
 export HOME=/config
 
 sleep 1
 
 filebot -script fn:amc \
+	--db thetvdb \
 	--output "/data/videos/" \
-	--db anidb
-	--def animeFormat="Anime/{n}/{n}.S{(episode.season ? s : 1).pad(2)}E{e.pad(2)}.{t}" \
 	--def movieFormat="Movies/{n} ({y})/{n} ({y})" \
+	--def seriesFormat="Anime/{n}/{'Season '+s}/{n} - {s00e00} - {t}" \
 	--log-file amc.log \
 	--action copy \
 	-non-strict "/data/torrents/completed" \
 	--def excludeList=amc.txt \
 	--def "exec=chown 1000:1000 -R '{output}'"
-
